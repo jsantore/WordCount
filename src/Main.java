96cb388cdb8d369860a4b6c206be13b48f50ce51
johns <jsantore@bridgew.edu>
2025-06-26 11:03:13 -0400

    initial wordcount function

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,9 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
 void main(){
@@ -12,17 +14,34 @@ void main(){
     System.out.println("What File do you want to count words in?");
     System.out.println("Short.txt or warpeace.txt");
     var userInput = inputReader.nextLine();
+    if (userInput.isEmpty()) {
+        var newInput = inputReader.nextLine();
+        userInput = newInput;
+    }
     var wordCount = countWords(userInput);
+
 //    System.out.println(wordCount);
 }
 
 HashMap<String, Integer> countWords(String filename){
-    var wordCount = new HashMap<String, Integer>();
-
+    var wordCountDict = new HashMap<String, Integer>();
+    List<String> fileLines = null;
     try {
-        var fileLines = Files.readAllLines(Paths.get(filename));
+        fileLines = Files.readAllLines(Paths.get(filename));
     } catch (IOException e) {
         System.out.println("File not found" + e.getMessage());
+        System.exit(1);
     }
-
+    for(var line: fileLines){
+        var cleanLine = line.replaceAll("\\p{Punct}", "");
+        var words = line.split("\\W+");
+        for(var word: words){
+            if(wordCountDict.containsKey(word)){
+                wordCountDict.put(word, wordCountDict.get(word) + 1);
+            }else{
+                wordCountDict.put(word, 1);
+            }
+        }
+    }
+    return wordCountDict;
 }
